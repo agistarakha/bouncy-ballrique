70b81034806f7071f0d0c3058aa015831ada7f98
agistarakha <agistarakhasnc@gmail.com>
2021-10-09 14:55:01 +0700

    problem-8 solved

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -5,12 +5,57 @@ using UnityEngine;
 public class Collectibles : MonoBehaviour
 {
 
+    private float respawnDuration = 3.0f;
+    private float timer = 0;
+    private BoxCollider2D collectibleCollider;
+    private bool isCollected;
+
+    void Awake()
+    {
+        isCollected = false;
+        collectibleCollider = GetComponent<BoxCollider2D>();
+    }
+
+
+    void Update()
+    {
+        if (isCollected)
+        {
+            if (timer < respawnDuration)
+            {
+                timer += Time.deltaTime;
+            }
+            else
+            {
+                if (GameManager.Instance.respawnAllowed)
+                {
+                    Respawn();
+
+                }
+            }
+
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Ball")
         {
             ScoreManager.score += 1;
-            Destroy(gameObject);
+            // gameObject.SetActive(false);
+            isCollected = true;
+            collectibleCollider.enabled = false;
+            transform.localScale = Vector3.zero;
         }
+    }
+
+    private void Respawn()
+    {
+        gameObject.transform.position = GameManager.Instance.GenerateSpawnPoint();
+        // gameObject.SetActive(true);
+        transform.localScale = Vector3.one;
+        collectibleCollider.enabled = true;
+        timer = 0;
+        isCollected = false;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager _instance = null;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+            return _instance;
+        }
+    }
     public GameObject collectiblePrefab;
+    public bool respawnAllowed = true;
+
+    private GameObject ball;
+
 
     private float horizontalBoundary = 4f;
     private float verticalBoundary = 3f;
     // Start is called before the first frame update
     void Start()
     {
+        ball = FindObjectOfType<BallController>().gameObject;
         SpawnCollectibles();
     }
 
@@ -22,15 +39,40 @@ public class GameManager : MonoBehaviour
 
     void SpawnCollectibles()
     {
-        int collectibleCount = Random.Range(3, 9);
+        int collectibleCount = Random.Range(3, 8);
         for (int i = 0; i < collectibleCount; i++)
         {
-            Vector3 spawnPoint = new Vector3(
-                Random.Range(-horizontalBoundary, horizontalBoundary),
-                Random.Range(-verticalBoundary, verticalBoundary),
-                0
-            );
-            Instantiate(collectiblePrefab, spawnPoint, Quaternion.identity);
+            Vector3 spawnPoint = GenerateSpawnPoint();
+            GameObject spawnedPrefab = Instantiate(collectiblePrefab, spawnPoint, Quaternion.identity);
         }
     }
+
+    public Vector3 GenerateSpawnPoint()
+    {
+        bool isSpawnPointAllowed = false;
+        Vector3 spawnPoint = new Vector3();
+        while (!isSpawnPointAllowed)
+        {
+            spawnPoint = new Vector3(
+                    Random.Range(-horizontalBoundary, horizontalBoundary),
+                    Random.Range(-verticalBoundary, verticalBoundary),
+                    0
+                );
+            float xBallDistance = Mathf.Abs(spawnPoint.x) - Mathf.Abs(ball.transform.position.x);
+            float yBallDistance = Mathf.Abs(spawnPoint.y) - Mathf.Abs(ball.transform.position.y);
+
+            if (Mathf.Abs(xBallDistance) > 1)
+            {
+                isSpawnPointAllowed = true;
+            }
+            else if (Mathf.Abs(yBallDistance) > 1)
+            {
+                isSpawnPointAllowed = true;
+            }
+
+
+        }
+        return spawnPoint;
+    }
+
 }
