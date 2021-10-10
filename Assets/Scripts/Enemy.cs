07f9128e6434214100bf2c61e41705f687794252
agistarakha <agistarakhasnc@gmail.com>
2021-10-10 10:51:15 +0700

    problem-9 solved

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Invoke("InitialPush", 1);
+        InvokeRepeating("PushBall", 4, 4);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void PushBall()
+    {
+        rb.velocity = rb.velocity * 1.02f;
+    }
+    void InitialPush()
+    {
+        rb.velocity = new Vector2(4f, 4f);
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            GameOverManager.Instance.ShowGameOverUI();
+        }
+    }
+}
---
+++ Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public static GameOverManager _instance = null;
+    public static GameOverManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameOverManager>();
+            }
+            return _instance;
+        }
+    }
+
+    public GameObject gameOverPanel;
+    public Button restartButton;
+    public bool isGameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+        // restartButton = gameOverPanel.GetComponentInChildren<Button>();
+        // restartButton.onClick.AddListener(Restart);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ShowGameOverUI()
+    {
+        gameOverPanel.SetActive(true);
+        StartCoroutine(RestartScene());
+    }
+
+    IEnumerator RestartScene()
+    {
+        yield return new WaitForSeconds(2);
+        ScoreManager.score = 0;
+        SceneManager.LoadScene("Problem 9");
+    }
+
+
+
+}
