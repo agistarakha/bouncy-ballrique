c85133a13f3ac2a57ed2061a6872150503adc34d
agistarakha <agistarakhasnc@gmail.com>
2021-10-10 12:07:25 +0700

    problem-10 solved & adding build file

---
+++ Assets/Scripts/BackToProblem10.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackToProblem10 : MonoBehaviour
+{
+    private Button backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton = GetComponent<Button>();
+        backButton.onClick.AddListener(Back);
+    }
+
+    private void Back()
+    {
+        ScoreManager.score = 0;
+        SceneManager.LoadScene("Problem 10");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ButtonSceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonSceneLoader : MonoBehaviour
+{
+
+    private Button loadSceneButton;
+    private Text buttonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadSceneButton = GetComponent<Button>();
+        buttonText = GetComponentInChildren<Text>();
+        buttonText.text = gameObject.name;
+        loadSceneButton.onClick.AddListener(LoadProblemScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LoadProblemScene()
+    {
+        SceneManager.LoadScene(gameObject.name);
+    }
+}
